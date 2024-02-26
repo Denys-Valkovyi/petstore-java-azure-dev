ba0e132ebd293d9ec4bb98d02aa8278a013aee98
Denys Valkovyi <denys.valkovyi@cantire.com>
2024-02-26 12:41:26 +0100

    adjusted k6 settings

--- petstore/perfomance/constant-load.js
+++
@@ -1,7 +0,0 @@
-import http from 'k6/http';
-import { sleep } from 'k6';
-
-export default function () {
-    http.get('https://your-app.azurewebsites.net/api/endpoint');
-    sleep(1); // Adjust the sleep time as needed
-}
--- petstore/perfomance/sudden-spike.js
+++ petstore/perfomance/sudden-spike.js
@@ -3,13 +3,13 @@ import { sleep } from 'k6';
 
 export let options = {
     stages: [
-        { duration: '1m', target: 10 },    // Ramp-up to 10 users over 1 minute
-        { duration: '1m', target: 50 },    // Spike to 50 users in 1 minute
-        { duration: '5m', target: 50 },    // Stay at 50 users for 5 minutes
+        { duration: '1m', target: 50 },    // Ramp-up to 50 users over 1 minute
+        { duration: '1m', target: 200 },    // Spike to 200 users in 1 minute
+        { duration: '15m', target: 200 },    // Stay at 200 users for 15 minutes
     ],
 };
 
 export default function () {
-    http.get('https://your-app.azurewebsites.net/api/endpoint');
+    http.get('https://denys-petstorepetservice-eastus.azurewebsites.net/petstorepetservice/v2/pet/info');
     sleep(1);
 }
